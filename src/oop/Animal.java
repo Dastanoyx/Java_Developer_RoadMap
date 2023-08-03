f5500997abc7ac1471585a1be93078cc6c494f9a
Yasin Ghariani <yassingharianipro@gmail.com>
2023-08-03 18:17:19 +0100

    OOP encapsulation

--- src/oop/Animal.java
+++ src/oop/Animal.java
@@ -1,9 +1,42 @@
 package oop;
 
 public class Animal {
-    public String name;
+    //here for inheritance
+    /*public String name;
     public String age;
-    public int nrOfEyes;
+    public int nrOfEyes;*/
+
+    // here for encapsulation
+    // encapsulation is private properties combined with public getters and setters
+    private String name;
+    private int age;
+    private int nrOfEyes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+
+    public int getNrOfEyes() {
+        return nrOfEyes;
+    }
+
+    public void setNrOfEyes(int nrOfEyes) {
+        this.nrOfEyes = nrOfEyes;
+    }
 
     public void eat(){
         System.out.println("Animal eating");
