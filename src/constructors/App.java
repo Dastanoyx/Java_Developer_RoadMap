c56687df5816c5f5490e99385c91c72c189de895
Yasin Ghariani <yassingharianipro@gmail.com>
2023-08-06 22:31:29 +0100

    Constructors & inheritance

--- src/constructors/App.java
+++ src/constructors/App.java
@@ -3,9 +3,15 @@ package constructors;
 public class App {
     public static void main(String[] args) {
         //check the target after u run it ^_
-        Student s1 = new Student("John", 20);
+        //Student s1 = new Student("John", 20);
 
         // this() = constructor in the same class
         // super() = constructor in the parent class
+
+        // Constructors and inheritance
+        Car car = new Car();
+
+        // Java adds super() when there is no constructor call explicitly!
+        // (Only works if the parent has a no-args constructor)
     }
 }
---
+++ src/constructors/Vehicle.java
@@ -0,0 +1,11 @@
+package constructors;
+
+public class Vehicle {
+   /* public Vehicle(){
+        System.out.println("In Vehicle constructor!");
+    }*/
+
+    public Vehicle(String name){
+        System.out.println("In Vehicle constructor!");
+    }
+}
