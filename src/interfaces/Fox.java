1f463b62c1afc95cc75f9467bc18e279f58f566d
Yasin Ghariani <yassingharianipro@gmail.com>
2023-08-10 15:41:11 +0100

    interfaces

---
+++ src/interfaces/Fox.java
@@ -0,0 +1,21 @@
+package interfaces;
+
+public class Fox extends Mammal implements Predator, Prey{
+    @Override
+    public void catchAndEat() {
+        System.out.println("Fox is catching and eating prey");
+    }
+
+
+
+    @Override
+    public void runAndHide() {
+        System.out.println("Running and hiding from predator");
+    }
+
+    @Override
+    public void run() {
+        //Predator.super.run();
+        System.out.println("Fox is running");
+    }
+}
