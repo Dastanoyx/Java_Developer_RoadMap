23a8a699e07471cc6d75d890f29edd9d3e0d8f35
Yasin Ghariani <yassingharianipro@gmail.com>
2023-08-24 00:38:31 +0100

    Generics

---
+++ src/genericsandcollections/BookBag.java
@@ -0,0 +1,14 @@
+package genericsandcollections;
+
+public class BookBag {
+
+    private Book[] content;
+
+    public Book[] getContent() {
+        return content;
+    }
+
+    public void setContent(Book[] content) {
+        this.content = content;
+    }
+}
--- src/interfaces/Mammal.java
+++ src/interfaces/Mammal.java
@@ -1,7 +1,7 @@
 package interfaces;
 
-public class Mammal extends Animal{
-public void breathe(){
-    System.out.println("Mammal breathing.");
-}
+public  class Mammal extends Animal{
+    public void breathe(){
+        System.out.println("Mammal breathing.");
+    }
 }
